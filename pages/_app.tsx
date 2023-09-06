00be5ac43bc43139d0b589796e730b7a0dacf28b
msaunders-twilio <msaunders.twilio@gmail.com>
2023-09-06 11:15:35 -0400

    Add demo for disclosure component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Theme} from '@twilio-paste/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Theme.Provider theme="evergreen">
+      <Component {...pageProps} />
+    </Theme.Provider>
+  )
 }
---
+++ pages/demo2.tsx
@@ -0,0 +1,47 @@
+import {
+  Disclosure, 
+  DisclosureHeading, 
+  DisclosureContent
+} from '@twilio-paste/core/disclosure';
+import { Button } from '@twilio-paste/core/button';
+import { Flex } from '@twilio-paste/core/flex';
+
+
+export default function Demo2() {
+  return (
+    <>
+      <main>
+        <div 
+          style={{
+            width: '50%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        >
+          <p>Disclosure spike</p>
+        
+          <Disclosure variant="contained">
+            <DisclosureHeading as="h2" variant="heading20">
+              <div 
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <div>
+                  Disclosure Heading
+                </div>
+                <Button variant="secondary">
+                  Duplicate
+                </Button>
+              </div>
+            </DisclosureHeading>
+            <DisclosureContent>Disclosure content</DisclosureContent>
+          </Disclosure>
+          </div>
+      </main>
+    </>
+  )
+}
